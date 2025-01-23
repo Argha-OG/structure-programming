a4cd74af2046f67b0c026cdc62f7fbd5bfb8fe6a
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-01-23 22:54:44 +0600

    p-1

---
+++ problem-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    
+    // Read the input string
+    scanf("%s", name);
+    
+    // Print the greeting message
+    printf("Hello, %s\n", name);
+    
+    return 0;
+}
