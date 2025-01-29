34a56d96094123c5cccbf913b62bb801b3c4e90e
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-01-30 02:40:06 +0600

    a

--- learning/1.c
+++ learning/1.c
@@ -9,7 +9,7 @@
 //type2
 // int age = 30;
 
-//output1
+//Change value of variables
 int main() {
     
     int age = 30;
@@ -17,6 +17,19 @@ int main() {
 
     age = 32;
     printf("\nMy new age is","%d", age);
+
+
+    return 0;
+}
+
+//Assign variable to another variable
+int main() {
+    
+    int firstNumber = 30;
+    printf("First number is %d", firstNumber);
+
+    int secondNumber = firstNumber;
+    printf("\nSecond number is %d", secondNumber);
 
 
     return 0;
