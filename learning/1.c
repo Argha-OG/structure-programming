9357bc2be43790bf1ad97b92daf277136b45b424
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-01-30 02:33:08 +0600

    a

---
+++ learning/1.c
@@ -0,0 +1,23 @@
+// variables and printing output
+
+#include <stdio.h>
+
+//type1
+// int age;
+// age = 30;
+
+//type2
+// int age = 30;
+
+//output1
+int main() {
+    
+    int age = 30;
+    printf("My age is %d", age);
+
+    age = 32;
+    printf("\nMy new age is","%d", age);
+
+
+    return 0;
+}
