3534c4e406e02c7c70db9ee59cb6592118b80d40
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-02-18 02:32:21 +0600

    User Input Update

--- learnings/4.userinput.c
+++ learnings/4.userinput.c
@@ -1,3 +1,44 @@
 // User Input in C
 
 #include <stdio.h> 
+int main () {
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your age is %d", age);
+
+    return 0;
+}
+
+// Take Double as input 
+#include <stdio.h>
+
+int main() {
+
+    double number;
+    char alphabet;
+    
+    printf("Enter Double input: ");
+    scanf("%lf", &number);
+
+    printf("Enter Character input: ");
+    scanf("\n%c", &alphabet);
+
+    printf("Here is the double: %lf", number,"/nHere is Character: %c", alphabet);
+    return 0;
+}
+
+// Take Multiple Inputs 
+#include <stdio.h>
+int main() {
+
+    double number;
+    char alphabet;
+
+    printf("Enter Input: ");
+    scanf("%lf %c", &number, &alphabet);
+
+    printf("Here is the double: %lf", number);
+    printf("\nHere is the character: %c", alphabet);
+}
