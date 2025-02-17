b6c0ce1021bce3518e38ec6d20d2c0063e1dbb5b
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-02-18 00:33:48 +0600

    variables update

--- learnings/1.introduction.c
+++ learnings/1.introduction.c
@@ -1,4 +1,6 @@
-// Step by step video tutorials to learn C Programming for absolute beginners!
+//  Programiz
+
+//  Step by step video tutorials to learn C Programming for absolute beginners!
 
 // In this video, we will introduce you to the exciting world of C programming. We will see why C is a good choice for beginners to start their programming journey. Then, we will create our first C program.
 
--- learnings/2.variables.c
+++ learnings/2.variables.c
@@ -12,12 +12,11 @@ int age = 30;
 //Change value of variables
 int main() {
     
-    int age = 30;
+    int age = 25;
     printf("My age is %d", age);
 
     age = 32;
     printf("\nMy new age is","%d", age);
-
 
     return 0;
 }
