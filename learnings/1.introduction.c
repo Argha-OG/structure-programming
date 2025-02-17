3008f2f42d106e0dbd2382755514ff6da1af5ea6
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-02-18 00:24:58 +0600

    complete introduction

---
+++ learnings/1.introduction.c
@@ -0,0 +1,16 @@
+// Step by step video tutorials to learn C Programming for absolute beginners!
+
+// In this video, we will introduce you to the exciting world of C programming. We will see why C is a good choice for beginners to start their programming journey. Then, we will create our first C program.
+
+// What is C programming?
+// Should you learn C?
+// How to run a C Program? 
+// Writing our first C program
+
+#include <stdio.h>
+int main() {
+
+    printf("Hello World!");
+   
+    return 0;
+}
