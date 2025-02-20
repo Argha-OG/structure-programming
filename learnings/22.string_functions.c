5df3fc4011af1d1f6f72845af0d8d2ba7a6e8574
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-02-20 23:43:21 +0600

    Strings Function

--- learnings/22.string_functions.c
+++ learnings/22.string_functions.c
@@ -1,5 +1,81 @@
-//Strings
-// strlen() function
-// 03:20 strcpy() function
-// 05:02 strcat() function
-// 06:32 strcmp() function
+//  Strings Function
+//  strlen() function
+//  strcpy() function
+//  strcat() function
+//  strcmp() function
+
+// String
+#include <stdio.h>
+
+int main() {
+
+        char lang[] = "Argha";
+
+        printf("%s", lang);
+
+    return 0;
+}
+
+//  strlen() function
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+        char lang[] = "Argha";
+
+        printf("%s", lang);
+
+        printf("\nLength: %zu", strlen(lang));
+
+    return 0;
+}
+
+//  strcpy() function
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+        char lang[] = "Argha";
+
+        char name[strlen(lang)];
+
+        strcpy(name, lang);
+
+        printf("%s", name);
+
+        // printf("\nLength: %zu", strcpy(lang));
+
+    return 0;
+}
+
+//  strcat() function
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+        char lang[] = "Argha ";
+        char name[] = "Biswas";
+        strcat(lang, name);
+
+        printf("%s", lang);
+
+    return 0;
+}
+
+//  strcmp() function
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+        char lang[] = "Argha ";
+        char name[] = "Biswas";
+        int result = strcmp(lang, name);
+
+        printf("%d", result);
+
+    return 0;
+}
