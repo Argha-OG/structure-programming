29ac77984772c65eeff22ef3f73101791fc4b21c
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-02-22 03:20:23 +0600

    introduction

--- learnings/01.introduction.c
+++ learnings/01.introduction.c
@@ -2,8 +2,6 @@
 
 //  Step by step video tutorials to learn C Programming for absolute beginners!
 
-// In this video, we will introduce you to the exciting world of C programming. We will see why C is a good choice for beginners to start their programming journey. Then, we will create our first C program.
-
 // What is C programming?
 // Should you learn C?
 // How to run a C Program? 
