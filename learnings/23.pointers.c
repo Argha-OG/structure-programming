bce66f9e7bcff121539399937bbb9005e1a4000f
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-02-22 02:45:16 +0600

    C Pointers

--- learnings/23.pointers.c
+++ learnings/23.pointers.c
@@ -5,3 +5,65 @@
 //  Common Mistakes in Pointers 
 
 // C Pointers
+#include <stdio.h>
+
+int main () {
+
+    int age = 25;
+
+    printf("%p", &age);
+
+    int* ptr = &age;
+
+
+    printf("\n%p", ptr);
+    
+
+    return 0;
+}
+
+//  Access Value using Pointers
+#include <stdio.h>
+
+int main () {
+
+    int age = 25;
+
+    int* ptr = &age;
+
+
+    printf("Address: %p\n", ptr);
+    printf("Value: %d\n", *ptr);
+    
+
+    return 0;
+}
+
+//  Change Value using Pointers
+#include <stdio.h>
+
+int main () {
+
+    int age = 25;
+
+    int* ptr = &age;
+
+
+    *ptr = 32;
+
+    printf("New Value: %d\n", *ptr);
+    
+
+    return 0;
+}
+
+//  Common Mistakes in Pointers
+
+    int *ptr; //types value, avoid this mistake
+
+    int* ptr; // write form 
+
+    ptr = number; // invalid
+    *ptr = &number; // invalid
+    ptr = &number; // valid
+    *ptr = number; // valid
